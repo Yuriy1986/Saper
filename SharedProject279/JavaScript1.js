8592d44e789077af58a1c6521e2ac1e00338aaa1
DESKTOP-EPNMGGB\Никитосик <yuriy.lazar1986@gmail.com>
2018-05-12 02:46:32 +0300

    no message

--- SharedProject279/JavaScript1.js
+++ SharedProject279/JavaScript1.js
@@ -1,23 +1,25 @@
-﻿function Saper(row, col, w, h, level) {    //class Saper
+﻿function Saper(row, col, w, h, level) {
     this.row = row;
     this.col = col;
     this.sumCell = this.row * this.col;
     this.w = w;
     this.h = h;
-    this.level = level; //1 level - 10% min, 2 level - 20% min, 3 level - 30% min
-    this.firstStep = true;  //to determine the first step
-    this.countFreeCell; //for functiom win
-    this.countStep = 0; //for functiom win
+    //  1 level - 10% min, 2 level - 20% min, 3 level - 30% min.
+    this.level = level;
+    //  To determine the first step.
+    this.firstStep = true;
+    this.countFreeCell; 
+    this.countStep = 0; 
     this.clock = 0;
     this.timerId;
-    this.minesLeft; //mine - flags
+    this.minesLeft; 
 
     this.startGame =
         function () {
-            //create gamefield (15 - margin, 5 - between buttons)
+            //  Create gamefield (15 - margin, 5 - between buttons).
             $("<div/>", { id: "canv" }).appendTo("body").css({
-                "width": (30 + this.col * (this.w + 1) - 1),
-                "height": (30 + this.row * (this.h + 1) - 1),
+                "width": 30 + this.col * (this.w + 1) - 1,
+                "height": 30 + this.row * (this.h + 1) - 1
             });
 
             if ($("#canv").width() >= innerWidth || $("#canv").height() >= innerHeight) {
@@ -32,96 +34,97 @@
                     "top": innerHeight / 2 - $("#canv").height() / 2
                 });
             }
-            $("#canv").contextmenu(this.stubContextmenu); //click right button mouse
+            //  Click right button mouse.
+            $("#canv").contextmenu(this.stubContextmenu); 
 
-            //create buttons
+            //  Create buttons.
             for (var i = 0; i < this.row; i++) {
                 for (var j = 0; j < this.col; j++) {
-                    var temp = i * this.col + j + 1;   //for numbering id///////////////////////////////
+                    var temp = i * this.col + j + 1;
 
                     $("<button/>", { class: "but" }).appendTo("#canv").css({
                         "width": this.w,
                         "height": this.h,
                         "top": 15 + i * (this.h + 1),
-                        "left": 15 + j * (this.w + 1),
-                    }).attr("id", "el" + temp);//.text(temp)
+                        "left": 15 + j * (this.w + 1)
+                    }).attr("id", "el" + temp);
 
                     $("#el" + temp).click(this.step.bind(this, $("#el" + temp)));   //click left button mouse
                     $("#el" + temp).contextmenu(this.flag.bind(this, $("#el" + temp))); //click right button mouse
                 }
             }
-        }   //end StartGame
+        };
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Saper.prototype.timer =
         function () {
-            $("#time").text("Время: " + (++this.clock));
-        }
+            $("#time").text("Время: " + ++this.clock);
+        };
 
-Saper.prototype.stubContextmenu =   //stub right click mouse
+    //  Stub right click mouse.
+Saper.prototype.stubContextmenu =   
     function (event) {
-        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-     //   event.returnValue = false;
-     //   event.preventDefault();
-    }
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    };
 
 Saper.prototype.step =
     function () {
-        var idClick = parseInt(arguments[0].attr("id").substr(2), 10);  //get the id click
+        var idClick = parseInt(arguments[0].attr("id").substr(2), 10);
 
-        if (this.firstStep) //first step
+        if (this.firstStep) 
         {
             this.firstStep = false;
-            //arrangement of mines
+            //  Arrangement of mines.
             var mine = Math.round(this.sumCell * this.level / 10);
             this.minesLeft = mine;
             $("#mine").text(this.minesLeft);
             this.countFreeCell = this.sumCell - mine;
-            // alert(this.countFreeCell);
+
             for (var i = 0; i < mine;) {
                 var temp = Math.round(Math.random() * (this.sumCell - 1) + 1);
-                if (temp == idClick || $("#el" + temp).prop('mine'))
+                if (temp === idClick || $("#el" + temp).prop('mine'))
                     continue;
                 $("#el" + temp).prop('mine', true);
                 i++;
             }
-          this.timerId= setInterval(this.timer.bind(this), 1000);
+            this.timerId = setInterval(this.timer.bind(this), 1000);
         }
-        if ($("#el" + idClick).prop('mine'))    //check for loss
+        //  Check for loss.
+        if ($("#el" + idClick).prop('mine'))    
         {
             this.loss(idClick);
             return;
         }
 
-        if (this.count(idClick) != 0)	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (this.count(idClick) !== 0)	
         {
             $("#el" + idClick).css({
                 "background-color": "white"
             }).prop('step', true).text(this.count(idClick));
-            $("#el" + idClick).off();  
-            $("#el" + idClick).contextmenu(this.stubContextmenu); //click right button mouse
+            $("#el" + idClick).off();
+            $("#el" + idClick).contextmenu(this.stubContextmenu); 
             this.countStep++;
         }
         else
-            this.wave(idClick); //wave algorithm
+            this.wave(idClick); 
 
-        //check for winnings
+        //  Сheck for winnings.
         if (this.win()) {
             $("#canv").animate({
                 opacity: '0.8'
             }, 100, handler);
+        }
 
-            function handler() {
-                alert("Вы выиграли");
-            }
+        function handler() {
+            alert("Вы выиграли");
         }
     };
 
-Saper.prototype.wave =  //wave algorithm
+Saper.prototype.wave =  
     function (idClick) {
 
         for (var i = idClick - 1; i <= idClick + 1; i++) {
-            if (i < 1 || i > this.sumCell || Math.ceil(i / this.col) != Math.ceil(idClick / this.col))////////////
+            if (i < 1 || i > this.sumCell || Math.ceil(i / this.col) !== Math.ceil(idClick / this.col))
                 continue;
 
             for (var j = i - this.col; j <= i + this.col; j += this.col) {
@@ -132,13 +135,13 @@ Saper.prototype.wave =  //wave algorithm
                 $("#el" + j).css({
                     "background-color": "white"
                 }).prop('step', true);
-                $("#el" + j).off();  ////////////////////////////////////////////////////
-                $("#el" + j).contextmenu(this.stubContextmenu); //click right button mouse
+                $("#el" + j).off();  
+                $("#el" + j).contextmenu(this.stubContextmenu); 
 
                 this.countStep++;
 
                 var temp = this.count(j);
-                if (temp == 0)
+                if (temp === 0)
                     this.wave(j);
                 else {
                     $("#el" + j).text(temp);
@@ -146,12 +149,12 @@ Saper.prototype.wave =  //wave algorithm
             }
         }
     };
-
-Saper.prototype.count =  //count mines around the perimeter
+    //  Count mines around the perimeter.
+Saper.prototype.count =  
     function (idClick) {
         var countMines = 0;
         for (var i = idClick - 1; i <= idClick + 1; i++) {
-            if (i < 1 || i > this.sumCell || Math.ceil(i / this.col) != Math.ceil(idClick / this.col))
+            if (i < 1 || i > this.sumCell || Math.ceil(i / this.col) !== Math.ceil(idClick / this.col))
                 continue;
 
             for (var j = i - this.col; j <= i + this.col; j += this.col) {
@@ -164,11 +167,11 @@ Saper.prototype.count =  //count mines around the perimeter
         return countMines;
     };
 
-Saper.prototype.flag =  //label mines
+Saper.prototype.flag =  
     function () {
         if (this.firstStep)
             return;
-        var idClick = parseInt(arguments[0].attr("id").substr(2), 10);  //get the id click
+        var idClick = parseInt(arguments[0].attr("id").substr(2), 10);  
 
         if ($("#el" + idClick).prop('flag')) {
             $("#mine").text(++this.minesLeft);
@@ -176,7 +179,7 @@ Saper.prototype.flag =  //label mines
             $("#el" + idClick).css({
                 "background-image": 'none'
             });
-            $("#el" + idClick).click(this.step.bind(this, $("#el" + idClick)));   //click left button mouse
+            $("#el" + idClick).click(this.step.bind(this, $("#el" + idClick)));   
             $("#el" + idClick).prop('flag', false);
         }
         else {
@@ -193,7 +196,7 @@ Saper.prototype.flag =  //label mines
         this.stubContextmenu;
     };
 
-Saper.prototype.loss =  //loss
+Saper.prototype.loss =  
     function (idClick) {
         clearInterval(this.timerId);
         $("#mine").prop('disabled', true);
@@ -201,7 +204,7 @@ Saper.prototype.loss =  //loss
 
         for (var i = 1; i <= this.sumCell; i++) {
             $("#el" + i).off();
-            if (i == idClick) {
+            if (i === idClick) {
                 $("#el" + idClick).css({
                     "background-image": 'url("Images/fire(lose).gif")',
                     "background-size": "contain",
@@ -236,13 +239,9 @@ Saper.prototype.loss =  //loss
         }
     };
 
-Saper.prototype.win =    //win
+Saper.prototype.win =    
 function () {
-    //for (var i = 1; i <= this.row * this.col; i++) {
-    //    if (!$("#el" + i).prop('step') && !$("#el" + i).prop('mine'))
-    //        return false;
-    //}
-    if (this.countStep != this.countFreeCell)
+    if (this.countStep !== this.countFreeCell)
         return false;
     else {
         clearInterval(this.timerId);
@@ -263,4 +262,3 @@ function () {
         return true;
     }
 };
-
